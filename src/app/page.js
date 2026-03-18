2a59d4acea5e3ab96b1168a90c79b609b8bf021a
James Nebou <jamesnebou34@gmail.com>
2026-03-18 00:26:57 -0300

    Adiciona page.js para a rota principal

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+// app/page.js
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Bem-vindo ao NexaWi!</h1>
+      <p>Esta é a página inicial do seu aplicativo.</p>
+    </main>
+  );
+}
