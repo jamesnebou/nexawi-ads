ed3e66452494c2ae93fc22f99fc49b4a7960eaf9
James Nebou <jamesnebou34@gmail.com>
2026-03-23 00:28:04 -0300

    Atual

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      // ESTA SEÇÃO ABAIXO É A MAIS IMPORTANTE PARA VERIFICAR
       boxShadow: {
         'green-glow': '0 0 15px rgba(34, 197, 94, 0.6)', // Verde padrão (green-500) com 60% de opacidade
       },
