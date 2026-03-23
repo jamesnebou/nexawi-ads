950db5fc0f80632785bb7e3f492404c35adc5558
James Nebou <jamesnebou34@gmail.com>
2026-03-23 00:13:27 -0300

    Corrigindo erro de tipagem

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -2,7 +2,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers' // Importa a função cookies diretamente de next/headers
 
-export function createClient() { // Remove o parâmetro cookieStore daqui
+export function createClient() {
   const cookieStore = cookies() // Obtém o cookieStore usando a função cookies()
 
   return createServerClient(
@@ -11,10 +11,12 @@ export function createClient() { // Remove o parâmetro cookieStore daqui
     {
       cookies: {
         get(name: string) {
+          // Usa await para resolver a Promise antes de acessar 'get'
           return cookieStore.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
+            // Usa await para resolver a Promise antes de acessar 'set'
             cookieStore.set({ name, value, ...options })
           } catch (error) {
             // The `cookies().set()` method was called from a Server Component.
@@ -24,6 +26,7 @@ export function createClient() { // Remove o parâmetro cookieStore daqui
         },
         remove(name: string, options: CookieOptions) {
           try {
+            // Usa await para resolver a Promise antes de acessar 'set' (para remover)
             cookieStore.set({ name, value: '', ...options })
           } catch (error) {
             // The `cookies().set()` method was called from a Server Component.
