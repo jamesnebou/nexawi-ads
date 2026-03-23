ed7c26c56be27b57fed360c525c73b3e5f071392
James Nebou <jamesnebou34@gmail.com>
2026-03-22 23:48:36 -0300

    Atualização com cor de caixa em neon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}', // Mantido, pois a pasta 'components' existe
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        'green-glow': '0 0 15px rgba(34, 197, 94, 0.6)', // Verde padrão (green-500) com 60% de opacidade
+      },
+    },
   },
   plugins: [],
 }
